188f5694124fb3e70c23cea5986fce823317e358
Mike Dilger <mike@mikedilger.com>
2014-06-18 20:27:27 +1200

    Restructured into a DepGraph struct with an impl

--- dglr.rs
+++ dglr.rs
@@ -1,5 +1,4 @@
 // Dependency Graph Library in Rust
-
 #![crate_type = "lib"]
 
 // For log related macros:
@@ -9,80 +8,118 @@ extern crate log;
 
 use std::collections::hashmap::{HashMap,HashSet};
 
-struct State<'a> {
-    output: Vec<String>,
-    remaining: HashSet<&'a str>,
+pub struct DepGraph {
+    pub dependencies: HashMap<String,Vec<String>>,
 }
 
-pub fn dglr_get_ordered_dependencies_of<'c>(node: &'c str,
-                          dependencies: &'c HashMap<&'c str,Vec<&'c str>>)
-                          -> Vec<String>
-{
-    let mut state = State {
-        output: Vec::new(),
-        remaining: HashSet::new(),
-    };
+struct WalkState {
+    remaining: HashSet<String>,
+    output: Vec<String>,
+}
 
-    // Scan the dependencies, and save all nodes in remaining
-    for (n, dependsOn) in dependencies.iter() {
-        debug!("preparing {}",n);
-        state.remaining.insert(n.clone());
-        for n2 in dependsOn.iter() {
-            debug!("preparing {}",n2);
-            state.remaining.insert(n2.clone());
+impl DepGraph {
+
+    pub fn new() -> DepGraph
+    {
+        DepGraph {
+            dependencies: HashMap::new(),
         }
     }
 
-    debug!("Recursing for the first time, with {}",node);
-    dglr_deps_of(node, &mut state, dependencies);
-    debug!("output is {}",state.output);
-    state.output
-}
+    pub fn add_dependency<'a>( &mut self,
+                                thing: &'a str,
+                                dependsOn: &'a str )
+    {
+        self.dependencies.insert_or_update_with(
+            String::from_str(thing),
+            vec![String::from_str(dependsOn)],
+            |_,v| { v.push(String::from_str(dependsOn)); }
+            );
+    }
 
-fn dglr_deps_of<'a>(node: &'a str,
-                   state: &mut State<'a>,
-                   dependencies: &'a HashMap<&'a str,Vec<&'a str>>)
-                   -> ()
-{
-    match dependencies.find(&node) {
+    pub fn add_dependencies<'a>( &mut self,
+                                  thing: &'a str,
+                                  dependsOn: Vec<&'a str> )
+    {
+        let newvec: Vec<String> = dependsOn.iter().map(
+            |s| String::from_str(*s)).collect();
 
-        None => {
-            debug!("{} has no dependencies",node);
-        },
+        self.dependencies.insert_or_update_with(
+            String::from_str(thing),
+            newvec.clone(),
+            |_,v| { v.push_all(newvec.as_slice()); }
+            );
+    }
 
-        Some(deplist) => {
-            debug!("Handling the dependencies of {}",node);
-            for n in deplist.iter() {
-                // If node was not yet visited, recurse into it
-                if state.remaining.contains(n) {
+    pub fn get_ordered_dependencies_of(&self, thing: &str)
+        -> Vec<String>
+    {
+        let mut state = WalkState {
+            output: Vec::new(),
+            remaining: HashSet::new(),
+        };
 
-                    debug!("Recursing for {}",n);
-                    dglr_deps_of(*n, state, dependencies);
-
-                    debug!("Appending {} to output",*n);
-                    state.output.push(String::from_str(*n));
-                    state.remaining.remove(n);
-                }
+        // Scan the dependencies, and save all nodes in remaining
+        for (n, dependsOn) in self.dependencies.iter() {
+            debug!("preparing {}",n);
+            state.remaining.insert(n.clone());
+            for n2 in dependsOn.iter() {
+                debug!("preparing {}",n2);
+                state.remaining.insert(n2.clone());
             }
-        },
+        }
+
+        debug!("Recursing for the first time, with {}",thing);
+        self.get_deps_of_recurse(&String::from_str(thing), &mut state);
+        debug!("output is {}",state.output);
+        state.output
+    }
+
+    fn get_deps_of_recurse(&self, thing: &String, state: &mut WalkState)
+    {
+        match self.dependencies.find(thing) {
+            None => {
+                debug!("{} has no dependencies",thing);
+            },
+
+            Some(deplist) => {
+                debug!("Handling the dependencies of {}",thing);
+                for n in deplist.iter() {
+                    // If thing was not yet visited, recurse into it
+                    if state.remaining.contains(n) {
+
+                        debug!("Recursing for {}",n);
+                        self.get_deps_of_recurse(n, state);
+
+                        debug!("Appending {} to output",n);
+                        state.output.push(n.clone());
+                        state.remaining.remove(n);
+                    }
+                }
+            },
+        }
+
     }
 }
 
 #[test]
 fn dglr_test() {
-    let mut map: HashMap<&str,Vec<&str>> = HashMap::new();
-    map.insert("a",vec!["b","c","g"]);
-    map.insert("b",vec!["d"]);
-    map.insert("c",vec!["e","m","g"]);
-    map.insert("e",vec!["f"]);
-    map.insert("g",vec!["h"]);
-    map.insert("h",vec!["i"]);
-    map.insert("i",vec!["j","k"]);
-    map.insert("k",vec!["l","m"]);
-    map.insert("m",vec!["n"]);
+    let mut depgraph: DepGraph = DepGraph::new();
 
-    let deps = dglr_get_ordered_dependencies_of("a",&map);
-    println!("Deps of a = {}",deps);
+    depgraph.add_dependencies("a",vec!["b","c","d"]);
+    depgraph.add_dependencies("b",vec!["d"]);
+    depgraph.add_dependencies("c",vec!["e","m","g"]);
+    depgraph.add_dependencies("e",vec!["f"]);
+    depgraph.add_dependencies("g",vec!["h"]);
+    depgraph.add_dependencies("h",vec!["i"]);
+    depgraph.add_dependencies("i",vec!["j","k"]);
+    depgraph.add_dependencies("k",vec!["l","m"]);
+    depgraph.add_dependencies("m",vec!["n"]);
+
+    let deps = depgraph.get_ordered_dependencies_of("a");
+
+    println!("Deps of i = {}",deps);
+
     assert!( deps == vec![String::from_str("d"),
                           String::from_str("b"),
                           String::from_str("f"),
@@ -97,7 +134,8 @@ fn dglr_test() {
                           String::from_str("g"),
                           String::from_str("c")] );
 
-    let deps2 = dglr_get_ordered_dependencies_of("i",&map);
+
+    let deps2 = depgraph.get_ordered_dependencies_of("i");
     println!("Deps of i = {}",deps2);
     assert!( deps2 == vec![String::from_str("j"),
                            String::from_str("l"),
