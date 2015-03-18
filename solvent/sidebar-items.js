406af345b316ea6e6887db14bad2c0ccc1104bad
Mike Dilger <mike@efx.co.nz>
2015-03-18 15:03:28 +1300

    Updated with 0.6.0

---
+++ solvent/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["DepGraph","This is the dependency graph."],["DepGraphIterator","This iterates through the dependencies of the DepGraph's target"]],"enum":[["SolventError",""]]});
