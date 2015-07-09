8b2d4d2c1101dba6aa516e0b0f7cbb0e8df6cc7b
Mike Dilger <mike@efx.co.nz>
2015-07-10 10:32:19 +1200

    doc update

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['log'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='log/struct.SetLoggerError.html' title='log::SetLoggerError'>SetLoggerError</a>",];implementors['solvent'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='solvent/enum.SolventError.html' title='solvent::SolventError'>SolventError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- solvent/sidebar-items.js
+++ solvent/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["DepGraph","This is the dependency graph."],["DepGraphIterator","This iterates through the dependencies of the DepGraph's target"]],"enum":[["SolventError",""]]});
+initSidebarItems({"enum":[["SolventError",""]],"struct":[["DepGraph","This is the dependency graph"],["DepGraphIterator","This iterates through the dependencies of the DepGraph's target"]]});
